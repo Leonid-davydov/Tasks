e5d2e422286f20b9a5bbb5308a18428aed96edb4
Leonid-davydov <lenya-davydov@mail.ru>
2022-04-19 21:51:03 +0300

    task 7

---
+++ src/Task_7/Main.java
@@ -0,0 +1,12 @@
+package Task_7;
+
+public class Main {
+    public static void main(String[] arg) {
+        recursion();
+        long[] arr = new long[Integer.MAX_VALUE];
+    }
+
+    static void recursion() {
+        recursion();
+    }
+}
